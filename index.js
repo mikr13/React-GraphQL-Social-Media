90bc73f64636413818b9cb957d8f7c34bac14d2a
Mihir Kumar <mihirpandey.13@gmail.com>
2020-04-07 18:29:58 +0530

    Definitions added, env added, graphql apollo connected

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+const { ApolloServer, PubSub } = require('apollo-server');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const typeDefs = require('./graphql/definitions');
+const resolvers = require('./graphql/resolvers');
+
+const pubsub = new PubSub();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req, pubsub })
+});
 
 mongoose
   .connect(process.env.MONGODB, {
@@ -9,4 +20,8 @@ mongoose
   })
   .then(() => {
     console.log('Mongo Connected Successfully');
+    return server.listen({ port: process.env.port });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
   });
